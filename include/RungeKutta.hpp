00089be1b7240b1fb5613ec25f3bc1d232099dfb
cfr2ak <cfr2ak@gmail.com>
2020-02-24 20:25:39 +0900

    Define the constructor

--- include/RungeKutta.hpp
+++ include/RungeKutta.hpp
@@ -12,10 +12,12 @@ class RK4
 {
 private:
     vector<float> y;
+    vector<float> t;
     float k1, k2, k3, k4;
+    float h;    // Step size
     void step();
 public:
-    RK4();
+    RK4(float y0, float t0, float h);
     void get();
 };
 
--- src/RungeKutta.cpp
+++ src/RungeKutta.cpp
@@ -3,14 +3,20 @@
 namespace gy
 {
 
-RK4::RK4()
+RK4::RK4(float y0, float t0, float h): h(h)
 {
-
+    y.push_back(y0);
+    t.push_back(t0);
 }
 
 void RK4::step()
 {
+    // Calculate k1~k4
 
+    auto y_next = y.back() + (k1 + 2.0f * k2 + 2.0f * k3 + k4) / 6.0f; 
+    y.push_back(y_next);
+
+    auto t_next = t.back() + h;
 }
 
 void RK4::get()
