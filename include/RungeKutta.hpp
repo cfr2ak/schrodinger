fe58e78c759e3cbbbdd663e5887976c4a6776c75
cfr2ak <cfr2ak@gmail.com>
2020-02-24 20:35:40 +0900

    Implement the stepper function
    Implement the RK4 algorithm

--- include/RungeKutta.hpp
+++ include/RungeKutta.hpp
@@ -1,6 +1,7 @@
 #ifndef GY_RUNGE_KUTTA_H
 #define GY_RUNGE_KUTTA_H
 
+#include <functional>
 #include <vector>
 
 using namespace std;
@@ -11,13 +12,13 @@ namespace gy
 class RK4
 {
 private:
-    vector<float> y;
-    vector<float> t;
-    float k1, k2, k3, k4;
-    float h;    // Step size
+    function<float(float, float)> f;
+    vector<float> y, t;
+    float k1, k2, k3, k4, h;
+
     void step();
 public:
-    RK4(float y0, float t0, float h);
+    RK4(float y0, float t0, float h, function<float(float, float)> f);
     void get();
 };
 
--- src/RungeKutta.cpp
+++ src/RungeKutta.cpp
@@ -3,7 +3,8 @@
 namespace gy
 {
 
-RK4::RK4(float y0, float t0, float h): h(h)
+RK4::RK4(float y0, float t0, float h, function<float(float, float)> f)
+: h(h), f(f)
 {
     y.push_back(y0);
     t.push_back(t0);
@@ -11,12 +12,19 @@ RK4::RK4(float y0, float t0, float h): h(h)
 
 void RK4::step()
 {
-    // Calculate k1~k4
+    auto tn = t.back();
+    auto yn = y.back();
 
-    auto y_next = y.back() + (k1 + 2.0f * k2 + 2.0f * k3 + k4) / 6.0f; 
+    k1 = h * f(tn, yn);
+    k2 = h * f(tn + h / 2.0f, yn + k1 / 2.0f);
+    k3 = h * f(tn + h / 2.0f, yn + k2 / 2.0f);
+    k4 = h * f(tn + h, yn + k3);
+
+    auto y_next = yn + (k1 + 2.0f * k2 + 2.0f * k3 + k4) / 6.0f; 
     y.push_back(y_next);
 
-    auto t_next = t.back() + h;
+    auto t_next = tn + h;
+    t.push_back(t_next);
 }
 
 void RK4::get()
